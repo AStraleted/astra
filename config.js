a53032957a94836ce2d6baa55e40dd265e95d6df
AStraleted <ovengameplov@gmail.com>
2026-08-14 09:03:58 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   // Можно оставить ключ в переменной окружения SMSCODEX_API_KEY.
   // Если принципиально хотите хранить всё в Git, вставьте полный ключ сюда.
   // В ПУБЛИЧНЫЙ репозиторий реальный ключ коммитить нельзя.
-  SMSCODEX_API_KEY: process.env.SMSCODEX_API_KEY || 'PASTE_FULL_API_KEY_HERE',
+  SMSCODEX_API_KEY: process.env.SMSCODEX_API_KEY || 'api_XX-Ys_FaL0eneaRk17IQ5vjoCFdEPS4pQrWXt4DJaeo',
 
   SMSCODEX_BASE_URL: 'https://smscodex.com',
   VK_SERVICE_CODE: 'vk',
